24588a0ca91b6c1a03c21c044d0b5ba2282fad1b
Anup Singh <anupbharadwaj54@gmail.com>
2021-06-12 23:58:03 +0530

    Express Session

--- ConfusionServer/app.js
+++ ConfusionServer/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -35,12 +37,20 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-08976-54321'));
+// app.use(cookieParser('12345-67890-08976-54321'));
+
+app.use(session({
+name:'session-id',
+secret:'12345-67890-08976-54321',
+saveUninitialized: false ,
+resave:false ,
+store: new FileStore()
+}));
 
 function auth(req,res,next){
-console.log(req.signedcookies);
+console.log(req.session);
 
-if(!req.signedCookies.user)
+if(!req.session.user)
 {
 var authHeader = req.headers.authorization;
   if(!authHeader)
@@ -52,10 +62,10 @@ var authHeader = req.headers.authorization;
    }
 var auth = new Buffer.from(authHeader.split('')[1],'base64').toString().split(':');
 var username = auth[0];
-var password = auth[1];
+var password = auth[ 1];
 
 if(username === 'admin' &&& password === 'password'){
-  res.cookie('user','admin',{signed:true})
+  req.session.user= 'admin';
    next();
 }
 else {
@@ -66,7 +76,7 @@ else {
    }
   }
   else {
-     if(req.signedCookies.user === 'admin'){
+     if(req.session.user === 'admin'){
        next();
      }
      else {
