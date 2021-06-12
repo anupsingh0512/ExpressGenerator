3ae7bce495cff86ead8faea5489ec64bb75df477
Anup Singh <anupbharadwaj54@gmail.com>
2021-06-12 17:33:11 +0530

    Express Rest API with Mongoose

--- ConfusionServer/models/dishes.js
+++ ConfusionServer/models/dishes.js
@@ -3,35 +3,42 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.currency;
 
-const commentSchema = new Schema({
-  rating:{
-    type:Number,
-    min:1,
-    max:5,
-    required:true
-  },
-  comment;{
-    type:String,
-    required:true
-  },
-   comments:[ commentSchema ]
-},{
-    timestamps:true
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false
+    },
+    comments:[commentSchema]
+}, {
+    timestamps: true
 });
 
-const dishSchema = new Schema({
-  name: {
-    type:String,
-    required:true,
-    unique:true
-  },
-  description:{
-    type:String,
-    required:true
-  }
-},{
-timestamps:true
-});
 
 var Dishes = mongoose.model('Dish',dishschema);
 
